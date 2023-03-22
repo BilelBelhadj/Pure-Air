6250286846d7f215b8e5c202b1e8fb975502e418
BilelBelhadj <belhadj19bi@gmail.com>
2023-03-22 00:38:52 -0300

    activer le FAN avec le relai selon la temperature

--- src/main.cpp
+++ src/main.cpp
@@ -21,51 +21,66 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 //Constants
 const int CO_SEN = A3;
+const int FAN = 1;      //la broche relier au relai pour activer le ventilateur
 
 //Variables
 float tmp = 0, hum = 0;
 int   carbone = 0, etat = 0;
 
 
-void setup(){
-    //demarrer le moniteur serie avec le vitesse 9600
-    Serial.begin(9600); 
-    
+void setup()
+{
+    Serial.begin(9600);     //demarrer le moniteur serie avec le vitesse 9600
+    pinMode(FAN, OUTPUT);   //mode de fonctionnement de broche
+
     //Verifier l'existence du AHT20
     if (aht.begin()) {
         Serial.println("Found AHT20");
     } else {
         Serial.println("Didn't find AHT20");
     }
+
     //verifier l'existance du l'ecran OLED
     if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;);
     }
+
     //effacer l'ecran et configurer le couleur
     display.clearDisplay();
     display.setTextColor(WHITE);
+
     //connecter sur internet et MQTT
     wifiConnect();              
-    MQTTConnect(); 
-
+    MQTTConnect();
 }
 
 
-void loop(){
-    //continuer a ecouter s'il y'a des RPC
-    ClientMQTT.loop(); 
+void loop()
+{
+    ClientMQTT.loop(); //continuer a ecouter s'il y'a des RPC
 
+    /******************************* Capture des donnees *******************************/
     //AHT20 data 
     aht.getEvent(&humidity, &temp);
     tmp = temp.temperature;
     hum = humidity.relative_humidity;
+    carbone = analogRead(CO_SEN);       //CO data
 
-    //CO data
-    carbone = analogRead(CO_SEN);
-    
-    //effacer l'ecran
-    display.clearDisplay();
+
+
+    /******************************* Gerer les actuateurs *******************************/
+    if (tmp > 26)
+    {
+        digitalWrite(FAN, HIGH);
+    }else{
+        digitalWrite(FAN, LOW);
+    }
+
+
+
+    /******************************* Affichage des donnees *******************************/
+    display.clearDisplay();  //effacer l'ecran
 
     //display temperature
     display.setTextSize(1);
@@ -91,7 +106,10 @@ void loop(){
     display.setCursor(0, 45);
     display.print(String(carbone));
 
-    //envoyer les donnees sur thingsboard
+
+
+    /******************************* envoyer les donnees sur thingsboard *******************************/
+
     appendPayload("CO2", carbone);
     appendPayload("Temperature", temp.temperature);
     appendPayload("Himidite", humidity.relative_humidity);
