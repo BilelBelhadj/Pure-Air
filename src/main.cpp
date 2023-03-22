118bf2fcc5816917efd8c5cfe0ed0164e312206d
BilelBelhadj <belhadj19bi@gmail.com>
2023-03-22 00:13:45 -0300

    ajouter la library du Ecran OLED et afficher les donnees sur l'ecran

--- src/main.cpp
+++ src/main.cpp
@@ -7,101 +7,83 @@ Version   : 0.0.1
 
 #include <Arduino.h>
 #include <Adafruit_AHTX0.h> 
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 Adafruit_AHTX0 aht;
 sensors_event_t humidity, temp;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-/************************Hardware Related Macros************************************/
-#define         MG_PIN                       (A0)     //define which analog input channel you are going to use
-#define         BOOL_PIN                     (2)
-#define         DC_GAIN                      (8.5)   //define the DC gain of amplifier
+//Constants
+const int CO_SEN = A3;
 
-/***********************Software Related Macros************************************/
-#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
-#define         READ_SAMPLE_TIMES            (5)     //define the time interval(in milisecond) between each samples in
-                                                     //normal operation
-
-/**********************Application Related Macros**********************************/
-//These two values differ from sensor to sensor. user should derermine this value.
-#define         ZERO_POINT_VOLTAGE           (0.220) //define the output of the sensor in volts when the concentration of CO2 is 400PPM
-#define         REACTION_VOLTGAE             (0.030) //define the voltage drop of the sensor when move the sensor from air into 1000ppm CO2
-
-/*****************************Globals***********************************************/
-float           CO2Curve[3]  =  {2.602,ZERO_POINT_VOLTAGE,(REACTION_VOLTGAE/(2.602-3))};
-                                                   
-float MGRead(int mg_pin)
-{
-    int i;
-    float v=0;
-
-    for (i=0;i<READ_SAMPLE_TIMES;i++) {
-        v += analogRead(mg_pin);
-        delay(READ_SAMPLE_INTERVAL);
-    }
-    v = (v/READ_SAMPLE_TIMES) *5/1024 ;
-    return v;
-}
+//Variables
+float tmp = 0, hum = 0;
+int   carbone = 0, etat = 0;
 
 
-int  MGGetPercentage(float volts, float *pcurve)
-{
-   if ((volts/DC_GAIN )>=ZERO_POINT_VOLTAGE) {
-      return -1;
-   } else {
-      return pow(10, ((volts/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
-   }
-}
-
-
-void setup()
-{
+void setup(){
+    //demarrer le moniteur serie avec le vitesse 9600
     Serial.begin(9600); 
-
+    
     //Verifier l'existence du AHT20
     if (aht.begin()) {
         Serial.println("Found AHT20");
     } else {
         Serial.println("Didn't find AHT20");
     }
-    
-    pinMode(BOOL_PIN, INPUT);                        //set pin to input
-    digitalWrite(BOOL_PIN, HIGH);                    //turn on pullup resistors
+    //verifier l'existance du l'ecran OLED
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+    }
+    //effacer l'ecran et configurer le couleur
+    display.clearDisplay();
+    display.setTextColor(WHITE);
 
-   Serial.print("MG-811 Demostration\n");
 }
 
-void loop()
-{
-    int percentage;
-    float volts;
 
-    volts = MGRead(MG_PIN);
-    Serial.print( "SEN0159:" );
-    Serial.print(volts);
-    Serial.print( "V           " );
+void loop(){
 
-    percentage = MGGetPercentage(volts,CO2Curve);
-    Serial.print("CO2:");
-    if (percentage == -1) {
-        Serial.print( "<400" );
-    } else {
-        Serial.print(percentage);
-    }
-
-    Serial.print( "ppm" );
-    Serial.print("\n");
-
-    if (digitalRead(BOOL_PIN) ){
-        Serial.print( "=====BOOL is HIGH======" );
-    } else {
-        Serial.print( "=====BOOL is LOW======" );
-    }
-
-    Serial.print("\n");
-    
+    //AHT20 data 
     aht.getEvent(&humidity, &temp);
-    Serial.println(temp.temperature);
-    Serial.println(humidity.relative_humidity);
+    tmp = temp.temperature;
+    hum = humidity.relative_humidity;
 
-    delay(2000);
+    //CO data
+    carbone = analogRead(CO_SEN);
+    
+    //effacer l'ecran
+    display.clearDisplay();
+
+    //display temperature
+    display.setTextSize(1);
+    display.setCursor(0,0);
+    display.print("Temp:");
+    display.setTextSize(2);
+    display.setCursor(0,10);
+    display.print(String(tmp));
+    
+    //display humidity
+    display.setTextSize(1);
+    display.setCursor(65, 0);
+    display.print("HUM: ");
+    display.setTextSize(2);
+    display.setCursor(65, 10);
+    display.print(String(hum));
+
+    //display quantite du CO2
+    display.setTextSize(1);
+    display.setCursor(0, 35);
+    display.print("CO2: ");
+    display.setTextSize(2);
+    display.setCursor(0, 45);
+    display.print(String(carbone));
+   
+    delay(5000);
 }
